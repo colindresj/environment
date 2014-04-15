02914d6eb0bf200513fdcf1864b283e90b0d518f
JC <jorge.e.colindres@gmail.com>
2014-04-14 23:09:06 -0400

    update sublime script

--- scripts/sublime.sh
+++ scripts/sublime.sh
@@ -2,12 +2,11 @@
 
 subl_settings="${HOME}/Library/Application Support/Sublime Text 2"
 
-# Symlink Sublime Text 2
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
-
 # Set Sublime user preferences
 user_prefs="Preferences.sublime-settings"
-user_keys="Default (OSX).sublime-keymap"
+
+# TODO update user keymap for close on slash
+user_keys="Default\ (OSX).sublime-keymap"
 cp "$SETTINGS/sublime/$user_prefs" "$subl_settings/Packages/User/$user_prefs"
 cp "$SETTINGS/sublime/$user_keys" "$subl_settings/Packages/User/$user_keys"
 
@@ -18,10 +17,10 @@ cp "$SETTINGS/sublime/$close_1" "$subl_settings/Packages/User/$close_1"
 cp "$SETTINGS/sublime/$close_2" "$subl_settings/Packages/User/$close_2"
 
 # Installing Package Control
-pkg_control="Package Control.sublime-package"
 pkg_url="https://sublime.wbond.net/Package%20Control.sublime-package"
-curl -L -o "$subl_settings/Installed Packages/$pkg_control" "$pkg_url"
+curl -o $subl_settings/Installed Packages/Package\ Control.sublime-package "$pkg_url"
 
+# TODO improve packages not installed through package control
 # Set packages that should be auto-installed on first open
 pkg_settings="Package Control.sublime-settings"
 cp "$SETTINGS/sublime/$pkg_settings" "$subl_settings/Packages/User/$pkg_settings"
