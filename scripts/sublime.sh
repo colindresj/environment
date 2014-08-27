3c3a33eaba330e90187d8b6dbda3d414856cbc2e
JC <jorge.e.colindres@gmail.com>
2014-08-27 12:07:53 -0400

    Remove close on slash python script

--- scripts/sublime.sh
+++ scripts/sublime.sh
@@ -5,16 +5,9 @@ subl_settings="${HOME}/Library/Application Support/Sublime Text 2"
 # Set Sublime user preferences
 user_prefs="Preferences.sublime-settings"
 
-# TODO update user keymap for close on slash
 user_keys="Default\ (OSX).sublime-keymap"
 cp "$SETTINGS/sublime/$user_prefs" "$subl_settings/Packages/User/$user_prefs"
 cp "$SETTINGS/sublime/$user_keys" "$subl_settings/Packages/User/$user_keys"
-
-# Close tag on slash
-close_1="close_tag_on_slash.py"
-close_2="close_tag_on_slash.pyc"
-cp "$SETTINGS/sublime/$close_1" "$subl_settings/Packages/User/$close_1"
-cp "$SETTINGS/sublime/$close_2" "$subl_settings/Packages/User/$close_2"
 
 # Installing Package Control
 pkg_url="https://sublime.wbond.net/Package%20Control.sublime-package"
